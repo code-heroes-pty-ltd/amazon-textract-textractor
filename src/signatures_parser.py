4b35d8c9e1c8afc4ed356c2b91d60b7e076b64ea
Paulina Szklarska <paulina@codeheroes.com.au>
2021-07-28 15:35:17 +1000

    parse top up forms

--- src/signatures_parser.py
+++ src/signatures_parser.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import math
 import json
 import csv
@@ -5,13 +6,16 @@ import csv
 key_full_name = 'Full Name'
 key_date = 'Date'
 key_email = 'Email'
+key_right_column = 'right'
+key_left_column = 'left'
 
 
 def parse(file_name):
     with open(file_name, 'r') as file:
         response = json.load(file)
 
-    signature_data = extract_signature_data(response)
+    is_top_up = is_file_top_up(file_name)
+    signature_data = extract_signature_data(response, is_top_up)
     grouped_signature_data = group_by_page_column(signature_data)
     signatures = convert_to_list(grouped_signature_data)
 
@@ -26,20 +30,21 @@ def parse(file_name):
             writer.writerow(row)
 
 
-def extract_signature_data(response):
-    signature_data = []
+def is_file_top_up(file_name):
+    return 'Top Up' in file_name
+
+
+def extract_signature_data(response, is_top_up):
+    signatures_list = []
     for responsePage in response:
         blocks = responsePage['Blocks']
         for block in blocks:
             if block['BlockType'] == "LINE":
-                left_signature_data = find_left_signature(block)
-                if left_signature_data is not None:
-                    signature_data.append(left_signature_data)
-                else:
-                    right_signature_data = find_right_signature(block)
-                    if right_signature_data is not None:
-                        signature_data.append(right_signature_data)
-    return signature_data
+                signature_data = find_signature_by_form_type(block, is_top_up)
+                if signature_data is not None:
+                    signatures_list.append(signature_data)
+
+    return signatures_list
 
 
 def group_by_page_column(signature_data):
@@ -66,37 +71,38 @@ def convert_to_list(grouped_signature_data):
     return signatures
 
 
-def find_left_signature(block):
-    signature_top = 0.42459
-    signature_left = 0.09459
-    date_top = 0.45682
-    date_left = 0.09847
-    email_top = 0.50061
-    email_left = 0.09494
-
-    return find_signature(block, date_left, date_top, email_left, email_top, signature_left, signature_top, 'left')
+def find_signature_by_form_type(block, is_top_up):
+    if is_top_up:
+        return find_top_up_form_right_signature(block) or find_top_up_form_left_signature(block)
+    else:
+        return find_application_form_right_signature(block) or find_application_form_left_signature(block)
 
 
-def find_right_signature(block):
-    signature_top = 0.42458
-    signature_left = 0.52781
-    date_top = 0.45682
-    date_left = 0.52781
-    email_top = 0.50047
-    email_left = 0.52882
-
-    return find_signature(block, date_left, date_top, email_left, email_top, signature_left, signature_top, 'right')
+def find_application_form_left_signature(block):
+    return find_signature(block, application_form_left_column_constraints, key_left_column)
 
 
-def find_signature(block, date_left, date_top, email_left, email_top, signature_left, signature_top, column):
+def find_application_form_right_signature(block):
+    return find_signature(block, application_form_right_column_constraints, key_right_column)
+
+
+def find_top_up_form_left_signature(block):
+    return find_signature(block, top_up_form_left_column_constraints, key_left_column)
+
+
+def find_top_up_form_right_signature(block):
+    return find_signature(block, top_up_form_right_column_constraints, key_right_column)
+
+
+def find_signature(block, constraints, column):
     box = block['Geometry']['BoundingBox']
     top_box = box['Top']
     left_box = box['Left']
-    if are_close(top_box, signature_top, left_box, signature_left):
+    if are_close(top_box, constraints.full_name_top, left_box, constraints.full_name_left):
         return map_to_dict(key_full_name, block, column)
-    if are_close(top_box, date_top, left_box, date_left):
+    if are_close(top_box, constraints.date_top, left_box, constraints.date_left):
         return map_to_dict(key_date, block, column)
-    if are_close(top_box, email_top, left_box, email_left):
+    if are_close(top_box, constraints.email_top, left_box, constraints.email_left):
         return map_to_dict(key_email, block, column)
 
 
@@ -105,4 +111,51 @@ def map_to_dict(key, block, column):
 
 
 def are_close(top_a, top_b, left_a, left_b):
-    return math.isclose(top_a, top_b, abs_tol=0.001) & math.isclose(left_a, left_b, abs_tol=0.001)
+    return math.isclose(top_a, top_b, abs_tol=0.0015) & math.isclose(left_a, left_b, abs_tol=0.0015)
+
+
+@dataclass
+class SignatureConstraints:
+    full_name_top: float
+    full_name_left: float
+    date_top: float
+    date_left: float
+    email_top: float
+    email_left: float
+
+
+application_form_right_column_constraints = SignatureConstraints(
+    full_name_top=0.42458,
+    full_name_left=0.52781,
+    date_top=0.45682,
+    date_left=0.52781,
+    email_top=0.50047,
+    email_left=0.52882,
+)
+
+application_form_left_column_constraints = SignatureConstraints(
+    full_name_top=0.42459,
+    full_name_left=0.09459,
+    date_top=0.45682,
+    date_left=0.09847,
+    email_top=0.50061,
+    email_left=0.09494,
+)
+
+top_up_form_right_column_constraints = SignatureConstraints(
+    full_name_top=0.72552,
+    full_name_left=0.53668,
+    date_top=0.79200,
+    date_left=0.53171,
+    email_top=0.75876,
+    email_left=0.53171,
+)
+
+top_up_form_left_column_constraints = SignatureConstraints(
+    full_name_top=0.72538,
+    full_name_left=0.09887,
+    date_top=0.79188,
+    date_left=0.09887,
+    email_top=0.75863,
+    email_left=0.09611,
+)
